c6d16eb6d1b344025bec689489919047497abd27
Chapada Host <contato@chapadahost.com.br>
2017-04-21 07:10:13 -0300

    Inserir e Alterar

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 	require_once("config.php");
 	
 	//CARREGA LISTA USUARIOS
-	$lista = Usuario::getList();
-
-	echo json_encode($lista);
+	//$lista = Usuario::getList();
+	//echo json_encode($lista);
 
 
 	//CARREGA UM USUÁRIO
@@ -15,13 +14,25 @@
 
 
 	/* SELECT
-
 	$sql = new Sql();
-
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 	echo json_encode($usuarios);
-
 	*/
+
+
+	/* CRIANDO UM NOVE USUARIO
+	$aluno = new Usuario("Davi", "3333");
+	$aluno->insert();
+
+	echo $aluno;
+	*/
+
+	$usuario = new Usuario();
+
+	$usuario->loadbyId(7);
+
+	$usuario->update("macaca", "8888");
+
+	echo $usuario;
 
  ?>
